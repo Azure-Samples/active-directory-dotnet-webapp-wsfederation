0d605e4d6e9b44a2b49cebb5be42e572cf1e2a06
vibronet <vibro@libero.it>
2014-04-28 17:17:04 -0700

    Initial commit
    Base ws-federation sign in sample for an MVC app and Azure AD

---
+++ WebApp-WSFederation-DotNet/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿//----------------------------------------------------------------------------------------------
+//    Copyright 2014 Microsoft Corporation
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//----------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+// The following using statements were added for this sample.
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.WsFederation;
+using Microsoft.Owin.Security;
+
+namespace WebApp_WSFederation_DotNet.Controllers
+{
+    public class AccountController : Controller
+    {
+        public void SignIn()
+        {
+            // Send a WSFederation sign-in request.
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(new AuthenticationProperties { RedirectUri = "/" }, WsFederationAuthenticationDefaults.AuthenticationType);
+            }
+        }
+        public void SignOut()
+        {
+            // Send a WSFederation sign-out request.
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                WsFederationAuthenticationDefaults.AuthenticationType, CookieAuthenticationDefaults.AuthenticationType);
+        }
+	}
+}
